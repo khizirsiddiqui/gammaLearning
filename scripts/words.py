65235e1aa6fe6540bb1638ae8a840156a2f453e2
Khizir Siddiqui <khizirsiddiqui@gmail.com>
2018-06-09 23:21:22 +0530

    Updated with os paths instead of relative paths

--- scripts/words.py
+++ scripts/words.py
@@ -1,9 +1,13 @@
 import json
 from random import randint
+import os
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+word_file = os.path.join(THIS_FOLDER, 'word_list.json')
 
 
 def rand_word():
-    with open('scripts/word_list.json', encoding="utf8") as f:
+    with open(word_file, encoding="utf8") as f:
         jsonData = json.load(f)
         word = jsonData[randint(0, len(jsonData)-1)]
         return word
